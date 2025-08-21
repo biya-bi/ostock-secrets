aad3fd3cf5c7779c7e22ad118f631f4e1aaa4f30
Paul Muriel Biya-Bi <paulmuriel.biyabi@gmail.com>
2025-08-20 22:13:30 -0400

    Improve int environment Postgres secret

--- scripts/create-postgres-secret.sh
+++ scripts/create-postgres-secret.sh
@@ -2,19 +2,50 @@
 
 set -eu
 
+script_dir=`realpath "$(dirname $0)"`
+project_dir=`realpath "${script_dir}/.."`
+
 namespace="ostock"
 
-secret_name="$1"
-secret_yaml_file="$2"
-url="$3"
-user="$4"
-password="$5"
+secret_name="postgres"
 
-kubectl create secret generic "${secret_name}" \
-    --from-literal=url="${url}" \
-    --from-literal=user="${user}" \
-    --from-literal=password="${password}" \
-    -o yaml \
-    --namespace="${namespace}" \
-    --dry-run=client \
-    | grep -v "\s*creationTimestamp:\s*null" > "${secret_yaml_file}"
+create_secret() {
+    local env="$1"
+
+    local secret_yaml="${project_dir}/${env}/sops-age/${secret_name}.yaml"
+    local credentials_dir="${NGUILAND_DATABASE_CONNECTION_STRING_DIR}"
+    local user=$(cat "${credentials_dir}/user")
+    local password=$(cat "${credentials_dir}/password")
+    local url=$(cat "${credentials_dir}/url")
+
+    kubectl create secret generic "${secret_name}" \
+        --from-literal=url="${url}" \
+        --from-literal=user="${user}" \
+        --from-literal=password="${password}" \
+        -o yaml \
+        --namespace="${namespace}" \
+        --dry-run=client \
+        | grep -v "\s*creationTimestamp:\s*null" > "${secret_yaml}"
+
+    cd "${project_dir}"
+
+    sops -e -i "${secret_yaml}"
+
+    printf "The %s secret was created in the %s file" "${secret_name}" "${secret_yaml}"
+}
+
+main() {
+    local expected_arg_count=1
+
+    if [ "$#" -lt "${expected_arg_count}" ]; then
+        printf "Error: Not enough arguments provided.\n"
+        printf "Usage: $0 <env>\n"
+        exit 1
+    fi
+
+    local env="$1"
+
+    create_secret "${env}"
+}
+
+main "$@"
